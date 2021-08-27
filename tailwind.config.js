af722914b22868c41e33014753329df1ed62f79e
Megha Paul <megha25072001@gmail.com>
2021-08-27 19:30:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors : {
+        primary : '#338EFF',
+        secondary : '#F31040',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
